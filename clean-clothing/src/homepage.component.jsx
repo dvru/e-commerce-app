a753390c387eeed545deef32092d494414140a47
dvru <damvaru4@gmail.com>
2020-06-01 20:28:46 -0500

    added homepage

--- clean-clothing/src/homepage.component.jsx
+++ clean-clothing/src/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
     <div className='homepage'>
-        <div className >
+        <div className='directory-menu'>
 
         </div>
     </div>
