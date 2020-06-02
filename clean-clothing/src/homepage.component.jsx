848b3fee39ad3393de5fde8416405be8d3194516
dvru <damvaru4@gmail.com>
2020-06-01 20:31:54 -0500

    added homepage

--- clean-clothing/src/homepage.component.jsx
+++ clean-clothing/src/homepage.component.jsx
@@ -18,5 +18,11 @@ const HomePage = () => {
                 </div>
             </div>
         </div>
+        <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
     </div>
 }
