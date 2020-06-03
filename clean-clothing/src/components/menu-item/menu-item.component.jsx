f1415c96de834e804332712592478430f7853b42
dvru <damvaru4@gmail.com>
2020-06-03 16:36:16 -0500

    added iamges

--- clean-clothing/src/components/menu-item/menu-item.component.jsx
+++ clean-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl}) => (
+const MenuItem = ({title, imageUrl, size}) => (
     <div style={{
         backgroundImage: `url(${imageUrl})`
     }} 
-    className='menu-item'>
+    className={`${size} menu-item`}
+    >
         <div className='content'>
             <h1 className='title'>{title}</h1>
             <span className='subtitle'>SHOP NOW</span>
