a5de6372ca437363172a45ec3b8dd39df9ce36b4
dvru <damvaru4@gmail.com>
2020-06-03 15:38:03 -0500

    added sass

--- clean-clothing/src/homepage.component.jsx
+++ clean-clothing/src/homepage.component.jsx
@@ -18,6 +18,12 @@ const HomePage = () => (
             </div>
             <div className='menu-item'>
                 <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
                     <h1 className='title'>WOMENS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
