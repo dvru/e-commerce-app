c1475cd93673da39fb7f05470c78b99bf5f4629b
dvru <damvaru4@gmail.com>
2020-06-05 17:42:34 -0500

    added signin

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -14,10 +15,10 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   )
-
 }
 
 export default App;
