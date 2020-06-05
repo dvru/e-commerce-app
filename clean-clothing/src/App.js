352eacb560707f99b358de210e4a2e42bb68a1f7
dvru <damvaru4@gmail.com>
2020-06-05 16:41:03 -0500

    added hats

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -3,17 +3,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return <div>
       <Route exact path='/' component={HomePage}/>
-      <Route path='/hats' component={HatsPage}/>
+      <Route path='/shop' component={ShopPage}/>
 
     </div>
 }
