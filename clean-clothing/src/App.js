898bcd2d1a3145fdaf96a267c960056fc44c5d4e
dvru <damvaru4@gmail.com>
2020-06-05 17:23:03 -0500

    added header

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 
 
