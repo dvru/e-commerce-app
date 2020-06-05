98d6edab06116f60403201d0ff89e5b74b26c54a
dvru <damvaru4@gmail.com>
2020-06-05 17:19:23 -0500

    added LOGO

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component';
 
 
 function App() {
-  return <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
+  )
+
 }
 
 export default App;
