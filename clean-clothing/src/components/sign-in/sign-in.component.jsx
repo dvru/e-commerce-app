0d00f558dbf4613a1dac91fa8140adae9dbde1ea
dvru <damvaru4@gmail.com>
2020-06-12 23:52:21 -0500

    submit

--- clean-clothing/src/components/sign-in/sign-in.component.jsx
+++ clean-clothing/src/components/sign-in/sign-in.component.jsx
@@ -28,7 +28,6 @@ render() {
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
-
                 <form onSubmit={this.handleSubmit}>
                     <input name="email" type="email" onChange={this.handleChange} value={this.state.email} required />
                     <label>Email</label>
