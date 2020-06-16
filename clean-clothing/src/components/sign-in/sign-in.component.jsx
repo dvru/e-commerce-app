80054bb50ddf140955ea2e7c542774edd4a86600
dvru <damvaru4@gmail.com>
2020-06-16 16:41:08 -0500

    signin

--- clean-clothing/src/components/sign-in/sign-in.component.jsx
+++ clean-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
 
 import './sign-in.styles.scss'
 
@@ -29,16 +30,23 @@ render() {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type="email" onChange={this.handleChange} value={this.state.email} required />
-                    <label>Email</label>
-                    <input 
+                    <FormInput 
+                    name="email" 
+                    type="email" 
+                    handleChange={this.handleChange} 
+                    value={this.state.email} 
+                    label="email"
+                    required />
+
+                    <FormInput 
                     name="password" 
                     type="password" 
                     value={this.state.email} 
-                    onChange={this.handleChange}
+                    handleChange={this.handleChange}
+                    label="password"
                     required 
                     />
-                    <label>Password</label>
+
                     <input type='submit' value='Submit Form'/> 
                 </form>
             </div>
