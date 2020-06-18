edac141ed969985fb5f933dc8597aa74148ebfc4
dvru <damvaru4@gmail.com>
2020-06-18 15:59:49 -0500

    redirect

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import { connect } from 'react-redux';
 
@@ -46,15 +46,29 @@ componentWillUnmount() {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route 
+          exact 
+          path='/signin' 
+          render={() => 
+          this.props.currentUser ? (
+          <Redirect to='/' />
+          ) : (
+            <SignInAndSignUpPage />
+            )
+          }
+          />
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
