03969b549fd9571b78a9274e3e2c9ca44716ddca
dvru <damvaru4@gmail.com>
2020-06-18 14:58:17 -0500

    added redux

--- clean-clothing/src/redux/user/user.reducer.js
+++ clean-clothing/src/redux/user/user.reducer.js
@@ -3,5 +3,13 @@ const INITIAL_STATE = {
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
-
+    switch (action.type) {
+        case 'SET_CURRENT_USER':
+            return{
+                ...state,
+                currentUser: action.payload
+            }
+        default:
+            return state;
+    }
 }
