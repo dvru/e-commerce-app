05bcf9b01334a63ab9432dd2ef33513000e10b8f
dvru <damvaru4@gmail.com>
2020-06-18 15:06:03 -0500

    added middleware

---
+++ clean-clothing/src/redux/store.js
@@ -0,0 +1,10 @@
+//between action and root reducer; catches action and console logs
+
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
