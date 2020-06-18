e723e1220a7d40f0a16766ba400a03251fff39dc
dvru <damvaru4@gmail.com>
2020-06-18 15:47:07 -0500

    connect

--- clean-clothing/src/App.js
+++ clean-clothing/src/App.js
@@ -1,42 +1,36 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
 
-constructor() {
-  super();
-  
-  this.state = {
-    currentUser: null
-  };
-}
 
 unsubcribeFromAuth = null;
   
 componentDidMount() {
+  const {setCurrentUser} = this.props
+
   this.unsubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
     if (userAuth) {
       const userRef = await createUserProfileDocument(userAuth);
 
       userRef.onSnapshot(snapShot => {
-        this.setState({
-          currentUser: {
+        setCurrentUser({
             id: snapShot.id,
             ...snapShot.data()
-          }
-        });
+          })
       })
     }
 
-    this.setState({currentUser: userAuth});
+    setCurrentUser(userAuth);
   });
 }ç
 
@@ -59,4 +53,8 @@ componentWillUnmount() {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
