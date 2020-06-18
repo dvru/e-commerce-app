e6d8baca557591f41347cc0a64eedf3e9c5105c6
dvru <damvaru4@gmail.com>
2020-06-18 16:14:02 -0500

    added cart icon

--- clean-clothing/src/components/header/header.component.jsx
+++ clean-clothing/src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils'
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import { ReactComponent as Logo } from '../../assets/flower.svg'
 import './header.styles.scss';
@@ -19,15 +20,16 @@ const Header = ({ currentUser }) => (
             <Link className='option'  to='/shop'>
             CONTACT
             </Link>
-            { currentUser ?
+            { currentUser ? (
                 <div className='option' onClick={() => auth.signOut()}> 
                 SIGN OUT 
                 </div>
-                :
+               ) : (
                 <Link className='option' to='/signin'>
                     SIGN IN
                 </Link>
-            }
+            )}
+            <CartIcon/>
         </div>
     </div>
 );
