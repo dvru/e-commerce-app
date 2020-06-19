7f0ece285723541a84d15b26031c1bdf2d9da2e9
dvru <damvaru4@gmail.com>
2020-06-18 19:08:57 -0500

    adding user selector

--- clean-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ clean-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,8 +11,12 @@ import './cart-dropdown.styles.scss';
 const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
-            {cartItems.map(cartItem => 
+            {cartItems.length ? (
+                cartItems.map(cartItem => (
                 <CartItem key={cartItem.id} item={cartItem} />
+                ))
+                ) : ( 
+                <span className='empty-message'>Your cart is empty</span>
                 )}
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
