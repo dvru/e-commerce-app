c0635e4481f7a13c6ce6c645e6e1502e2f07a91f
dvru <damvaru4@gmail.com>
2020-06-19 22:56:05 -0500

    collection routing and selector

--- clean-clothing/src/pages/collection/collection.component.jsx
+++ clean-clothing/src/pages/collection/collection.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CollectionItem from '../../components/collection-item/collection-item.component'
+// import CollectionItem from '../../components/collection-item/collection-item.component'
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
