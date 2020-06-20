da050c5c0325cd89c2eaf7d67bed38dbc06d92bb
dvru <damvaru4@gmail.com>
2020-06-19 22:55:37 -0500

    collection routing and selector

--- clean-clothing/src/redux/shop/shop.selectors.js
+++ clean-clothing/src/redux/shop/shop.selectors.js
@@ -17,7 +17,10 @@ export const selectCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => 
 createSelector (
-    [selectCollection],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
-)
+    [selectCollections],
+    collections => 
+        collections.find(
+            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+        )
+);
 
