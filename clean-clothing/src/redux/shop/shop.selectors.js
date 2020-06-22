1922922266ae1f8b54c8ddf4838e784facfca9c1
dvru <damvaru4@gmail.com>
2020-06-22 14:43:27 -0500

    added all of the collection

--- clean-clothing/src/redux/shop/shop.selectors.js
+++ clean-clothing/src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from 'reselect';
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
 
 const selectShop = state => state.shop;
 
@@ -18,9 +11,6 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => 
 createSelector (
     [selectCollections],
-    collections => 
-        collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
+    collections => collections[collectionUrlParam]
 );
 
