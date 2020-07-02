090ae1d6b63dc3a31b9f6437420f1b439d1fbb5c
dvru <damvaru4@gmail.com>
2020-07-02 15:09:26 -0500

    updated code to be pwa compliant, added service worker, https

--- clean-clothing/client/src/index.js
+++ clean-clothing/client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import {store, persistor} from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 
 import './index.css';
@@ -21,4 +22,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
--- clean-clothing/server.js
+++ clean-clothing/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const compression = require('compression')
+const enforce = require('express-sslify');
 
 if(process.env.NODE_ENV !== 'production')require('dotenv').config(); 
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 if(process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res) {
@@ -29,6 +31,10 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if (error) throw error;
     console.log('Server running on port ' + port);
+});
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (req, res) => {
